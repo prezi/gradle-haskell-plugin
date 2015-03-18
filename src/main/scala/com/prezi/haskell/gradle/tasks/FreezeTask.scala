c015329f383af1590d73ac4e9bcf0c66140029da
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2015-03-18 16:11:33 +0100

    No need to configure before freeze

--- src/main/scala/com/prezi/haskell/gradle/tasks/FreezeTask.scala
+++ src/main/scala/com/prezi/haskell/gradle/tasks/FreezeTask.scala
@@ -15,7 +15,6 @@ class FreezeTask extends CabalExecTask {
     needsConfigurationSet
     needsToolsSet
 
-    tools.get.cabalConfigure(cabalContext())
     tools.get.cabalFreeze(cabalContext())
   }
 }
