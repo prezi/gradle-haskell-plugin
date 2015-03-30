68415dcd4bc6b17d8332061eddab09c4f0bc049e
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2015-03-30 18:58:47 +0200

    Stripping reenabled

--- src/main/scala/com/prezi/haskell/gradle/external/HaskellTools.scala
+++ src/main/scala/com/prezi/haskell/gradle/external/HaskellTools.scala
@@ -23,8 +23,6 @@ class HaskellTools(executor : Action[ExecSpec] => ExecResult) {
       configFileArgs(ctx.configFile)
         ::: "configure"
         :: "--enable-tests"
-        :: "--disable-executable-stripping"
-        :: "--disable-library-stripping"
         :: "--package-db=clear"
         :: "--package-db=global"
         :: ctx.dependencies.map(_.asPackageDbArg)
