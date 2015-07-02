1fbb666818c762061c2de5a7b3a5af5ea44b9920
Dávid István Bíró <noise64@gmail.com>
2015-07-02 17:47:39 +0200

    compileTask upToDate check: when a sandbox dependency changes the compile task should run

--- src/main/scala/com/prezi/haskell/gradle/tasks/CompileTask.scala
+++ src/main/scala/com/prezi/haskell/gradle/tasks/CompileTask.scala
@@ -1,5 +1,7 @@
 package com.prezi.haskell.gradle.tasks
 
+import java.lang.Boolean
+
 import com.prezi.haskell.gradle.ApiHelper._
 import com.prezi.haskell.gradle.incubating.FunctionalSourceSet
 import com.prezi.haskell.gradle.model.Sandbox
@@ -8,6 +10,7 @@ import org.gradle.api.file.{FileVisitDetails, FileVisitor}
 import org.gradle.api.tasks.TaskAction
 
 import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
 
 /**
  * Executes cabal install with the proper sandbox chaining
@@ -18,36 +21,47 @@ class CompileTask extends CabalExecTask {
   dependsOn("sandbox")
   dependsOn("storeDependentSandboxes")
 
-  def attachToSourceSet(sourceSet: FunctionalSourceSet) = {
+  getOutputs.upToDateWhen{ _: AnyRef =>
+    (for {
+      storeDependentSandboxesTask <- Try { getProject.getTasksByName("storeDependentSandboxes", false).iterator().next() }
+      isAnySandboxUpdated <- Try { storeDependentSandboxesTask.asInstanceOf[StoreDependentSandboxes].isAnySandboxUpdated }
+    } yield isAnySandboxUpdated) match {
+      case Success(bool) => new Boolean(!bool)
+      case Failure(e) => throw new RuntimeException("Failed to get storeDependentSandboxes.anySandboxUpdated!", e)
+    }
+  }
 
-    for (lss <- sourceSet.asScala) {
-      lss.getSource.visit(new FileVisitor {
-        override def visitDir(fileVisitDetails: FileVisitDetails): Unit =
-        getInputs.dir(fileVisitDetails.getFile)
+    def attachToSourceSet(sourceSet: FunctionalSourceSet) = {
 
-        override def visitFile(fileVisitDetails: FileVisitDetails): Unit =
-          getInputs.file(fileVisitDetails.getFile)
-      })
+      for (lss <- sourceSet.asScala) {
+        lss.getSource.visit(new FileVisitor {
+          override def visitDir(fileVisitDetails: FileVisitDetails): Unit =
+            getInputs.dir(fileVisitDetails.getFile)
+
+          override def visitFile(fileVisitDetails: FileVisitDetails): Unit =
+            getInputs.file(fileVisitDetails.getFile)
+        })
+      }
+
+      dependsOn(sourceSet)
+      getOutputs.dir(getProject.getExtensions.getByType(classOf[Sandbox]).root)
     }
 
-    dependsOn(sourceSet)
-    getOutputs.dir(getProject.getExtensions.getByType(classOf[Sandbox]).root)
-  }
+    override def onConfigurationSet(cfg: Configuration): Unit =
+    {
+      dependsOn(cfg)
+    }
 
-  override def onConfigurationSet(cfg: Configuration): Unit = {
-    dependsOn(cfg)
-  }
+    @TaskAction
+    def run(): Unit = {
+      needsConfigurationSet
+      needsToolsSet
 
-  @TaskAction
-  def run(): Unit = {
-    needsConfigurationSet
-    needsToolsSet
-
-    val ctx = cabalContext()
-    tools.get.cabalInstall(ctx, depsOnly = true)
-    tools.get.cabalConfigure(ctx)
-    tools.get.cabalBuild(ctx)
-    tools.get.cabalCopy(ctx)
-    tools.get.cabalRegister(ctx)
+      val ctx = cabalContext()
+      tools.get.cabalInstall(ctx, depsOnly = true)
+      tools.get.cabalConfigure(ctx)
+      tools.get.cabalBuild(ctx)
+      tools.get.cabalCopy(ctx)
+      tools.get.cabalRegister(ctx)
+    }
   }
-}
--- src/main/scala/com/prezi/haskell/gradle/tasks/StoreDependentSandboxes.scala
+++ src/main/scala/com/prezi/haskell/gradle/tasks/StoreDependentSandboxes.scala
@@ -2,10 +2,11 @@ package com.prezi.haskell.gradle.tasks
 
 import java.io.File
 
-import com.prezi.haskell.gradle.model.{SandboxArtifact, ProjectSandboxStore, SandboxStore}
+import com.prezi.haskell.gradle.model.{SandBoxStoreResult, SandboxArtifact}
 import org.gradle.api.DefaultTask
 import org.gradle.api.artifacts.{Configuration, ResolvedDependency}
 import org.gradle.api.tasks.TaskAction
+
 import scala.collection.JavaConverters._
 
 class StoreDependentSandboxes extends DefaultTask with HaskellDependencies {
@@ -13,6 +14,10 @@ class StoreDependentSandboxes extends DefaultTask with HaskellDependencies {
   dependsOn("copySandFix")
 
   var sandFixPath: Option[File] = None
+
+  private var _isAnySandboxUpdated: Boolean = false
+
+  def isAnySandboxUpdated: Boolean = _isAnySandboxUpdated
 
   override def onConfigurationSet(cfg: Configuration): Unit = {
     dependsOn(cfg)
@@ -23,26 +28,46 @@ class StoreDependentSandboxes extends DefaultTask with HaskellDependencies {
     needsConfigurationSet
 
     getLogger.info("Storing dependent sandboxes for {}", getProject.getName)
-    for (dependency <- configuration.get.getResolvedConfiguration.getFirstLevelModuleDependencies.asScala) {
-      storeDependency(dependency)
-    }
+    val storeDependencyResults =
+      for (dependency <- configuration.get.getResolvedConfiguration.getFirstLevelModuleDependencies.asScala) yield {
+        storeDependency(dependency)
+      }
+
+    _isAnySandboxUpdated = storeDependencyResults.exists { _._2 == SandBoxStoreResult.Created }
   }
 
-  def storeDependency(dependency: ResolvedDependency, prefix: String = ""): Set[SandboxArtifact] = {
+  def storeDependency(dependency: ResolvedDependency, prefix: String = ""): (Set[SandboxArtifact], SandBoxStoreResult) = {
     getLogger.info("{}Dependency {}", prefix, dependency.getName)
-    val depSandboxes =
-      (for (child <- dependency.getChildren.asScala;
-            childDep <- storeDependency(child, prefix + "  "))
-       yield childDep).toSet
+
+    val (depSandboxes: Set[SandboxArtifact], aggregatedStoreResult: SandBoxStoreResult) =
+      foldStoreDependencyResults(
+        (for {
+          child <- dependency.getChildren.asScala
+          storeDependencyResult <- List(storeDependency(child, prefix + "  "))
+        } yield storeDependencyResult).toSet
+      )
 
     val sandboxes = dependency.getAllModuleArtifacts
       .asScala
       .map(artifact => new SandboxArtifact(artifact.getName, artifact.getFile))
       .toSet
-    for (sandbox <- sandboxes) {
-      store.store(sandbox, depSandboxes)
-    }
 
-    sandboxes
+    val sandboxStoreResults =
+      for (sandbox <- sandboxes) yield {
+        store.store(sandbox, depSandboxes)
+      }
+
+    (sandboxes,
+      if (sandboxStoreResults.contains(SandBoxStoreResult.Created) || aggregatedStoreResult == SandBoxStoreResult.Created)
+        SandBoxStoreResult.Created
+      else
+        SandBoxStoreResult.AlreadyExists)
   }
+
+  def foldStoreDependencyResults(results: Set[(Set[SandboxArtifact], SandBoxStoreResult)]): (Set[SandboxArtifact], SandBoxStoreResult) =
+    results.foldLeft((Set[SandboxArtifact](), SandBoxStoreResult.AlreadyExists.asInstanceOf[SandBoxStoreResult])) { (acc, elem) =>
+      (acc._1 ++ elem._1,
+        if (elem._2 == SandBoxStoreResult.Created) SandBoxStoreResult.Created
+        else SandBoxStoreResult.AlreadyExists)
+    }
 }
