e0336388fff30c00b3920fd482ba3cd3ffa85964
Dávid István Bíró <noise64@gmail.com>
2015-07-22 12:54:21 +0200

    Revert "fix getOrSetRootProjectProperty"
    This reverts commit e7e9d10c59001c91a07657d7647dac40fb32cfce.

--- src/main/scala/com/prezi/haskell/gradle/tasks/StoreDependentSandboxes.scala
+++ src/main/scala/com/prezi/haskell/gradle/tasks/StoreDependentSandboxes.scala
@@ -101,7 +101,7 @@ class StoreDependentSandboxes extends DefaultTask with HaskellDependencies {
   def getOrSetRootProjectProperty[V <: Object](propertyName: String, propertyValue: => V): V = {
     val rootProject = getProject.getRootProject
 
-    if (rootProject.getProperties.containsKey(propertyName)) {
+    if (rootProject.hasProperty(propertyName)) {
       rootProject.getProperties.get(propertyName).asInstanceOf[V]
     }
     else {
