90ca5ffb2995b5bab1a8e5e2459aa3b54d7920a4
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2015-10-21 15:07:59 +0200

    Stack sandbox zipping fix

--- src/main/scala/com/prezi/haskell/gradle/tasks/ZippedSandbox.scala
+++ src/main/scala/com/prezi/haskell/gradle/tasks/ZippedSandbox.scala
@@ -1,5 +1,7 @@
 package com.prezi.haskell.gradle.tasks
 
+import com.prezi.haskell.gradle.ApiHelper
+import org.gradle.api.Task
 import org.gradle.api.tasks.bundling.Zip
 
 /**
@@ -8,9 +10,5 @@ import org.gradle.api.tasks.bundling.Zip
 class ZippedSandbox extends Zip with HaskellProjectSupport with UsesSandbox {
 
     getDependsOn.addAll(getProject.getTasksByName("compileMain", false))
-
-    override def copy(): Unit = {
-        from(sandbox.root)
-        super.copy()
-    }
+    getProject.task("stackPath").doLast(ApiHelper.asClosure[Task](_ => from(sandbox.root)))
 }
