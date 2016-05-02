9d7fbd9a18fceceb31ad0cf4fca21ba5011655fb
Adam Rosenberger <rosadam@gmail.com>
2016-05-02 14:01:37 +0200

    Bump gradle to 2.13 - changes in wrapper are generated by gradle - followed gradle internal api changes

--- src/main/scala/com/prezi/haskell/gradle/model/HaskellSourceSet.scala
+++ src/main/scala/com/prezi/haskell/gradle/model/HaskellSourceSet.scala
@@ -2,6 +2,7 @@ package com.prezi.haskell.gradle.model
 
 import com.prezi.haskell.gradle.incubating.{FunctionalSourceSet, LanguageSourceSet, AbstractLanguageSourceSet}
 import org.gradle.api.internal.file.{DefaultSourceDirectorySet, FileResolver}
+import org.gradle.api.internal.file.collections.DefaultDirectoryFileTreeFactory;
 
 /**
  * Source set for Haskell projects
@@ -11,7 +12,7 @@ trait HaskellSourceSet extends LanguageSourceSet {
 }
 
 class DefaultHaskellSourceSet(name: String, parentName: FunctionalSourceSet, fileResolver: FileResolver)
-  extends AbstractLanguageSourceSet(name, parentName, "Haskell source", new DefaultSourceDirectorySet("source", fileResolver))
+  extends AbstractLanguageSourceSet(name, parentName, "Haskell source", new DefaultSourceDirectorySet("source", fileResolver, new DefaultDirectoryFileTreeFactory()))
   with HaskellSourceSet {
 
 }
